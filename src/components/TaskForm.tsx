d38e7ff601c8191a393eb1b5a3b47ca56b016431
Lazaro Osee <odiraoseelazaro@students.tukenya.ac.ke>
2025-01-03 18:07:26 +0300

    debug console

--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -24,6 +24,7 @@ export default function TaskForm({ groupId }: TaskFormProps) {
                 groupId,
             });
 
+            console.log('Task added successfully');
             setTitle('');
             setError(null);
         } catch (err) {
